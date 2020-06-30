8a310d0629a99396aed885a81b231706240e29ae
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2020-06-30 15:35:40 +0200

    Small fixes

--- taskconf/util/Logger.py
+++ taskconf/util/Logger.py
@@ -5,11 +5,11 @@ except ImportError:
   from pathlib import Path
 from multiprocessing import Lock
 import datetime
-
+import sys
 
 class Logger:
 
-    def __init__(self, log_path=None, file_name=None, parent_logger=None, module_name="general", replace=False):
+    def __init__(self, log_path=None, file_name=None, parent_logger=None, module_name="general", replace=False, terminal=None):
         """ Creates a logger.
 
         Args:
@@ -19,6 +19,7 @@ class Logger:
             module_name(str): The name of the module which uses this logger.
             replace(bool): True, if an existing log file should be replaced.
         """
+        self.terminal = terminal
         self._module_name = module_name
 
         if parent_logger is None:
@@ -50,43 +51,17 @@ class Logger:
             line += "[" + str(datetime.datetime.now()) + "]"
             line += "[" + logging.getLevelName(level) + "]"
             line += "[" + self._module_name + "]"
-            line += " " + message
+            line += " " + message.strip()
             line += "\n"
 
             self._mutex.acquire()
+            if self.terminal is not None:
+                self.terminal.write(line)
             self._file.write(line)
             self._file.flush()
             self._mutex.release()
         else:
             self._parent.log(message, level)
-
-    def log_confusion_matrix(self, level, confusion_matrix, classes):
-        """ Logs the given confusion matrix with proper alignment.
-
-        Args:
-            level(int): The log level of the message.
-            confusion_matrix(ndarray): The matrix to log.
-            classes(list): The class names.
-        """
-        cells = []
-
-        for i in range(len(classes)):
-            if i == 0:
-                row = ["Act \\ Pred"]
-                for class_name in classes:
-                    row.append(class_name)
-                cells.append(row)
-
-            row = [classes[i]]
-            for j in range(len(classes)):
-                row.append(str(int(confusion_matrix[i][j])))
-            cells.append(row)
-
-        for row in cells:
-            text = ""
-            for cell in row:
-                text += "{:11s}".format(cell) + "|"
-            self.log(text, level)
 
     def get_with_module(self, module_name):
         """ Clones this logger and sets a new module name.
